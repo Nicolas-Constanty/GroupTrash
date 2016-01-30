eb9804c808f31179533ad97dadabb29f98996c11
GasparQ <quentin.gasparotto@epitech.eu>
2016-01-30 14:39:45 +0100

    scene tuto avec ragdoll qui frappe ok (toujours dégueu en visuel)

---
+++ Assets/Scripts/TutoManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutoManager : MonoBehaviour {
+
+    public HitMeLight hitPart1;
+    public HitMeLight hitPart2;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (hitPart1.enoughtHitted() && hitPart2.enoughtHitted())
+            Application.LoadLevel("Scene_Nicolas");
+	}
+}
