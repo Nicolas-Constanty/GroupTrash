588de4c0c48c188db82e7137ff91b00730c0bd7a
Fernand <fernand.veyrier@epitech.eu>
2016-01-30 04:46:30 +0100

    Ajout loading image

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,10 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour {
 
+	[SerializeField]
+	private Image loadScreen;
+
+	public Sprite[] loadingScreens;
+
+	public void Start()
+	{
+		loadScreen.enabled = false;
+	}
+
 	public void LoadLevel()
+	{
+		if (loadingScreens.Length > 0)
+		{
+			loadScreen.enabled = true;
+			loadScreen.sprite = loadingScreens [Random.Range (0, loadingScreens.Length - 1)];
+			Invoke ("LoadScene", 2);
+		}
+		else
+		{
+			LoadScene ();
+		}
+	}
+
+	void LoadScene()
 	{
 		SceneManager.LoadScene (1);
 	}
